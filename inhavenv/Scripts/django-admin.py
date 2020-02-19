778631db66e98c141a82f0945dc3c4c82e5251c7
ji <alslahdk@naver.com>
2020-02-19 12:11:36 +0900

    first commit

---
+++ inhavenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alslahdk.desktop-h3vc5d2\onedrive\desktop\inha\inhavenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
