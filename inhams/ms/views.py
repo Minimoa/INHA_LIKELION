c8cfcb0954d904555a36dcf0d72688e311291387
ji <alslahdk@naver.com>
2020-02-22 18:28:30 +0900

    ji

--- inhams/ms/views.py
+++ inhams/ms/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
-<<<<<<< HEAD
-
 def detail(request):
     return render(request,'detail.html')
-=======
+    
 def new(request):
     return render(request, 'new.html')
->>>>>>> 3aa4d6c79fee01d74301f5a04f2e361392a36805
