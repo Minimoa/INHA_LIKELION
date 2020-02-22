27e7afb10a9bb499796dd8df76306060e606a313
ji <alslahdk@naver.com>
2020-02-22 23:45:35 +0900

    ji: search pagenation css favcon

--- inhams/ms/views.py
+++ inhams/ms/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404,get_list_or_404
 from .models import Word
+from django.core.paginator import Paginator
 # Create your views here.
 
-def home(req):
-    words = Word.objects
-    return render(req, 'index.html', {'words' : words})
+def home(req):  
+    words = Word.objects.all().order_by('-id') 
+    keyword = req.GET.get('search')
+    if keyword is not None:
+            words = Word.objects.filter(title__contains=keyword)
+    
+    paginator = Paginator(words,5) 
+    page = req.GET.get('page')
+    page_posts = paginator.get_page(page)
+
+    print(words)
+    return render(req,'index.html',{'page_posts':page_posts,'words':words}) 
+
+    #return render(req, 'index.html', {'words' : words})
 
 def detail(req, word_id):
     word = get_object_or_404(Word, id = word_id) # get_object_or_404(Word, pk = word_id)
